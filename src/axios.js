905c1f3de0f810dc7dc8d1425d29b77dc6d22402
VanyaShumylo <vanya.shumilo1@gmail.com>
2023-03-31 18:41:35 +0300

    test

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 import dotenv from 'dotenv'
 
-
-dotenv.config()
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: "https://backend-tm.herokuapp.com"
 })
 export default instance
